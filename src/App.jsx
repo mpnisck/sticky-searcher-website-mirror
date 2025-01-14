442e1df8562a2a32ff5dbff60de168d5c771a14a
YeonJu <mpnisck@gmail.com>
2025-01-14 21:08:17 +0900

    chore: tanstack query 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
       <h1 className="text-3xl font-bold underline text-mainSkyblue">
         Hello world!
       </h1>
+    </QueryClientProvider>
   );
 }
 
