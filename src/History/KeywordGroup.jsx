78e8e2e2aa4dc81b58ea2b121ab2595aa72a0398
YeonJu <mpnisck@gmail.com>
2025-01-21 16:02:09 +0900

    feat: 사용자 임의 history 분류 기능

--- src/History/KeywordGroup.jsx
+++ src/History/KeywordGroup.jsx
@@ -1,13 +1,32 @@
+import PropTypes from "prop-types";
+
 import HistoryItem from "./HistoryItem";
 import NewKeyword from "./NewKeyword";
 
-export default function KeywordGroup() {
+export default function KeywordGroup({
+  keyword,
+  keywords,
+  onDragStart,
+  onDrop,
+}) {
   return (
     <div>
-      <NewKeyword Keyword="New Keyword Group" />
+      <NewKeyword keyword={keyword} />
       <div className="flex justify-center items-center">
-        <HistoryItem />
+        <HistoryItem
+          keyword={keyword}
+          keywords={keywords}
+          onDragStart={onDragStart}
+          onDrop={onDrop}
+        />
       </div>
     </div>
   );
 }
+
+KeywordGroup.propTypes = {
+  keyword: PropTypes.string.isRequired,
+  keywords: PropTypes.array.isRequired,
+  onDragStart: PropTypes.func.isRequired,
+  onDrop: PropTypes.func.isRequired,
+};
