2e7d18a8d13db758af06e25e9e91f0bd24a0bcf0
JSLee <mac@macui-MacBookPro.local>
2025-01-26 19:50:08 +0900

    chore: daisyUI 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
@@ -9,5 +11,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
