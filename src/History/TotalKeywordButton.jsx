50b8a6f2a6c423efc9e79f155757e1b97984c54d
Under <52349132+josuk0212@users.noreply.github.com>
2025-01-27 12:45:24 +0900

    feat: history item 내 다수 키워드 토글 처리 (#17)
    * feat: 다수 키워드 show/hide  토글 기능
    
    * fix: 저장된 키워드 3개 이하 시 토글 문구 추가
    
    ---------
    
    Co-authored-by: JSLee <mac@macui-MacBookPro.local>

--- src/History/TotalKeywordButton.jsx
+++ src/History/TotalKeywordButton.jsx
@@ -1,13 +1,51 @@
 import PropTypes from "prop-types";
 
-export function TotalKeywordButton({ keyword = "..." }) {
+import SearchKeyword from "./SearchKeyword";
+
+export function TotalKeywordButton({ history }) {
+  const numberOfKeywords = history.keywords.length;
+
   return (
-    <button className="w-[25%] truncate shadow p-[5px] rounded-[5px]">
-      {keyword}
-    </button>
+    <div className="collapse collapse-arrow bg-base-200">
+      <input type="checkbox" />
+      <div className="collapse-title flex">
+        {history.keywords.map(({ keyword, count }, index) => {
+          return (
+            index < 3 && (
+              <SearchKeyword
+                key={index}
+                keyword={keyword}
+                count={count}
+              />
+            )
+          );
+        })}
+      </div>
+      <div className="collapse-content">
+        <div className="flex justify-center items-center">
+          {numberOfKeywords >= 4 ? (
+            history.keywords.map(({ keyword, count }, index) => {
+              return (
+                index >= 3 && (
+                  <SearchKeyword
+                    key={index}
+                    keyword={keyword}
+                    count={count}
+                  />
+                )
+              );
+            })
+          ) : (
+            <div className="text-sm text-gray-500 italic">
+              저장된 추가 키워드가 없습니다.
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
 
 TotalKeywordButton.propTypes = {
-  keyword: PropTypes.func,
+  history: PropTypes.object,
 };
