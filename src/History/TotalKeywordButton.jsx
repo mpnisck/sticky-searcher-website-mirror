b8efe1a86d2e89a645605d5ade779f2dfa78ed2c
JSLee <mac@macui-MacBookPro.local>
2025-01-27 10:25:53 +0900

    fix: 저장된 키워드 3개 이하 시 토글 문구 추가

--- src/History/TotalKeywordButton.jsx
+++ src/History/TotalKeywordButton.jsx
@@ -6,9 +6,7 @@ export function TotalKeywordButton({ history }) {
   const numberOfKeywords = history.keywords.length;
 
   return (
-    <div
-      className={`${numberOfKeywords > 3 ? "collapse collapse-arrow bg-base-200" : "collapse bg-base-200"}`}
-    >
+    <div className="collapse collapse-arrow bg-base-200">
       <input type="checkbox" />
       <div className="collapse-title flex">
         {history.keywords.map(({ keyword, count }, index) => {
@@ -24,18 +22,24 @@ export function TotalKeywordButton({ history }) {
         })}
       </div>
       <div className="collapse-content">
-        <div className="flex">
-          {history.keywords.map(({ keyword, count }, index) => {
-            return (
-              index >= 3 && (
-                <SearchKeyword
-                  key={index}
-                  keyword={keyword}
-                  count={count}
-                />
-              )
-            );
-          })}
+        <div className="flex justify-center items-center">
+          {numberOfKeywords >= 4 ? (
+            history.keywords.map(({ keyword, count }, index) => {
+              return (
+                index >= 3 && (
+                  <SearchKeyword
+                    key={index}
+                    keyword={keyword}
+                    count={count}
+                  />
+                )
+              );
+            })
+          ) : (
+            <div className="text-sm text-gray-500 italic">
+              저장된 추가 키워드가 없습니다.
+            </div>
+          )}
         </div>
       </div>
     </div>
