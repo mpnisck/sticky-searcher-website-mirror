6a5af75ccb5ba4a8d24c8b0e55cf6c0eef30d66d
YeonJu <mpnisck@gmail.com>
2025-01-29 00:03:35 +0900

    feat: webSite 로그인 기능 구현 (#25)
    * feat: webSite 로그인 기능 구현
    
    * refactor: 변수명 및 보일러플레이트 import 수정
    
    * refactor: 보일러플레이트 import 경로 수정
    
    * refactor: eslint 주석 제거

--- src/shared/Login.jsx
+++ src/shared/Login.jsx
@@ -1,8 +1,26 @@
-import { Link } from "react-router-dom";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
 import imgUrl from "../assets/sticky-seacher-logo.png";
+import { app } from "../firebase/firebase";
 
 export default function Login() {
+  const auth = getAuth(app);
+  const authData = async () => {
+    const provider = new GoogleAuthProvider();
+
+    const data = await signInWithPopup(auth, provider);
+
+    const user = data.user;
+    const email = user.email;
+    const accessToken = user.accessToken;
+
+    localStorage.setItem("userEmail", email);
+    localStorage.setItem("userAccessToken", accessToken);
+
+    window.localStorage.getItem("userEmail");
+    window.localStorage.getItem("userAccessToken");
+  };
+
   return (
     <div className="border rounded-[20px] h-screen flex flex-col justify-center items-center shadow">
       <img
@@ -13,12 +31,12 @@ export default function Login() {
       <h1 className="text-5xl font-thin mt-[30px] mb-[80px]">
         Sticky Searcher
       </h1>
-      <Link
-        to="/"
+      <button
+        onClick={authData}
         className="block text-2xl w-[45%] h-[70px] leading-[70px] flex-col align-center border rounded-lg shadow"
       >
         Google Login
-      </Link>
+      </button>
     </div>
   );
 }
